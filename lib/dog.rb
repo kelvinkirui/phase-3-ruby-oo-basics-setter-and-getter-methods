abb79c76963431309b901706b6c3794e7e58be6e
kk <+>
2023-05-17 23:19:50 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+    #setter methods
+    def name=(name)
+        @name = name
+      end
+
+      def breed
+        @breed
+      end
+
+#getter  methods
+      def breed=(breed)
+        @breed = breed
+      end
+
+    
+    def name
+        @name
+      end
+      
+     
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+#setter methods
+    def name=(name)
+        @name = name
+      end
+
+      def job=(job)
+        @job = job
+      end
+
+      
+ 
+      #getter methods
+      def name
+        @name
+      end
+
+      def job
+        @job
+      end
 
 end
